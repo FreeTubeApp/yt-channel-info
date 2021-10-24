41ddf9ff6d9e63e3abae6887cb25699498191d2c
ChunkyProgrammer <78101139+ChunkyProgrammer@users.noreply.github.com>
2021-10-24 10:33:34 -0400

    fix error when sorting by oldest

--- app/helper.js
+++ app/helper.js
@@ -97,8 +97,12 @@ class YoutubeGrabberHelper {
     }).map((item) => {
       return this.parseVideo(item, channelInfo)
     })
-    const playlistId = response.data[1].response.contents.twoColumnBrowseResultsRenderer.tabs[1].tabRenderer.content.sectionListRenderer.subMenu.channelSubMenuRenderer.playAllButton.buttonRenderer.navigationEndpoint.watchPlaylistEndpoint.playlistId
-    const playlistUrl = `https://www.youtube.com/watch?v=${latestVideos[0].videoId}&list=${playlistId}`
+    const channelSubMenuRenderer = response.data[1].response.contents.twoColumnBrowseResultsRenderer.tabs[1].tabRenderer.content.sectionListRenderer.subMenu.channelSubMenuRenderer
+    let playlistUrl = null
+    if ('playAllButton' in channelSubMenuRenderer) {
+      const playlistId = channelSubMenuRenderer.playAllButton.buttonRenderer.navigationEndpoint.watchPlaylistEndpoint.playlistId
+      playlistUrl = `https://www.youtube.com/watch?v=${latestVideos[0].videoId}&list=${playlistId}`
+    }
 
     return {
       items: latestVideos,
