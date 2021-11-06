056398df7720c89afb2ed648bc82e5b6dea36739
ChunkyProgrammer <78101139+ChunkyProgrammer@users.noreply.github.com>
2021-11-06 19:41:10 -0400

    Add tests for channel playlist urls

--- test/channelVideos.test.js
+++ test/channelVideos.test.js
@@ -1,5 +1,5 @@
 const ytch = require('../index')
-/* eslint no-undef: "off" */
+/* eslint no-undef: 'off' */
 describe('Getting channel videos', () => {
   test('Public channel with videos. Should return videos array (not empty)', () => {
     const parameters = { channelId: 'UCsBjURrPoezykLs9EqgamOA', channelIdType: 1 }
@@ -12,6 +12,27 @@ describe('Getting channel videos', () => {
     const parameters = { channelId: 'UCS-DgEvT4XuQsrrmI7iZVsA', channelIdType: 1 }
     return ytch.getChannelVideos(parameters).then((data) => {
       expect(data.items.length).toBe(0)
+    })
+  })
+
+  test('Public channel recent videos playlist', () => {
+    const parameters = { channelId: 'UCfMJ2MchTSW2kWaT0kK94Yw', channelIdType: 1, sortBy: 'newest' }
+    return ytch.getChannelVideos(parameters).then((data) => {
+      expect(data.playlistUrl.endsWith('list=UUfMJ2MchTSW2kWaT0kK94Yw')).toBe(true)
+    })
+  })
+
+  test('Public channel oldest videos playlist (non-existent)', () => {
+    const parameters = { channelId: 'UCfMJ2MchTSW2kWaT0kK94Yw', channelIdType: 1, sortBy: 'oldest' }
+    return ytch.getChannelVideos(parameters).then((data) => {
+      expect(data.playlistUrl).toBe(null)
+    })
+  })
+
+  test('Public channel popular videos playlist', () => {
+    const parameters = { channelId: 'UCX6OQ3DkcsbYNE6H8uQQuVA', channelIdType: 1, sortBy: 'popular' }
+    return ytch.getChannelVideos(parameters).then((data) => {
+      expect(data.playlistUrl.endsWith('list=PUX6OQ3DkcsbYNE6H8uQQuVA')).toBe(true)
     })
   })
 })
