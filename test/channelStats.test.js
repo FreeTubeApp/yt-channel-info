187078e19e2423a2955fe294fd939ad6e62e30ca
ChunkyProgrammer <78101139+ChunkyProgrammer@users.noreply.github.com>
2022-04-09 15:30:34 -0400

    improve test coverage (#69)

---
+++ test/channelStats.test.js
@@ -0,0 +1,10 @@
+const ytch = require('../index')
+/* eslint no-undef: "off" */
+describe('Channel stats', () => {
+  test('Channel stats', () => {
+    const parameters = { channelId: 'UCMO51vS4kaOSLqBD9bmZGIg', channelIdType: 1 }
+    return ytch.getChannelStats(parameters).then((data) => {
+      expect(data.joinedDate).toBe(0)
+    })
+  })
+})
--- test/channelVideos.test.js
+++ test/channelVideos.test.js
@@ -1,8 +1,32 @@
+const { expect } = require('@jest/globals')
 const ytch = require('../index')
 /* eslint no-undef: 'off' */
 describe('Getting channel videos', () => {
   test('Public channel with videos. Should return videos array (not empty)', () => {
     const parameters = { channelId: 'UCsBjURrPoezykLs9EqgamOA', channelIdType: 1 }
+    return ytch.getChannelVideos(parameters).then((data) => {
+      expect(data.items.length).not.toBe(0)
+    })
+  })
+
+  test('Get channel videos more', () => {
+    const parameters = { channelId: 'UCsBjURrPoezykLs9EqgamOA', channelIdType: 1 }
+    return ytch.getChannelVideos(parameters).then((data) => {
+      return ytch.getChannelVideosMore({ continuation: data.continuation }).then(vm => {
+        expect(vm.items.length).not.toBe(0)
+      })
+    })
+  })
+
+  test('Get Channel Videos oldest', () => {
+    const parameters = { channelId: 'UCsBjURrPoezykLs9EqgamOA', channelIdType: 1, sortBy: 'oldest' }
+    return ytch.getChannelVideos(parameters).then((data) => {
+      expect(data.items.length).not.toBe(0)
+    })
+  })
+
+  test('Get Channel Videos popular', () => {
+    const parameters = { channelId: 'UCsBjURrPoezykLs9EqgamOA', channelIdType: 1, sortBy: 'popular' }
     return ytch.getChannelVideos(parameters).then((data) => {
       expect(data.items.length).not.toBe(0)
     })
