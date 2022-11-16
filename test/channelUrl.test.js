b4ffee67eceb40ea8a40da1eb89b49cfbb76b23e
onion chunky <78101139+ChunkyProgrammer@users.noreply.github.com>
2022-11-15 19:50:33 -0500

    Allow channel tags + channel urls

--- test/channelUrl.test.js
+++ test/channelUrl.test.js
@@ -1,5 +1,13 @@
 const ytch = require('../index')
 describe('Standalone Mode: Channel URL Testing', () => {
+  test('Fallback URL', () => {
+    // https://www.youtube.com/channel/UCfMJ2MchTSW2kWaT0kK94Yw
+    const parameters = { channelId: 'UCfMJ2MchTSW2kWaT0kK94Yw', channelIdType: 0 }
+    return ytch.getChannelInfo(parameters).then((data) => {
+      expect(data.authorId).toBe('UCfMJ2MchTSW2kWaT0kK94Yw')
+    })
+  })
+
   test('Channel URL (ID-based)', () => {
     // https://www.youtube.com/channel/UCfMJ2MchTSW2kWaT0kK94Yw
     const parameters = { channelId: 'UCfMJ2MchTSW2kWaT0kK94Yw', channelIdType: 1 }
@@ -21,6 +29,22 @@ describe('Standalone Mode: Channel URL Testing', () => {
     const parameters = { channelId: 'YouTubeCreators', channelIdType: 3 }
     return ytch.getChannelInfo(parameters).then((data) => {
       expect(data.authorId).toBe('UCkRfArvrzheW2E7b6SVT7vQ')
+    })
+  })
+
+  test('Whole URL', () => {
+    // https://www.youtube.com/channel/UCfMJ2MchTSW2kWaT0kK94Yw
+    const parameters = { channelId: 'https://www.youtube.com/channel/UCfMJ2MchTSW2kWaT0kK94Yw', channelIdType: 5 }
+    return ytch.getChannelInfo(parameters).then((data) => {
+      expect(data.authorId).toBe('UCfMJ2MchTSW2kWaT0kK94Yw')
+    })
+  })
+
+  test('Channel tag URL (whole url passed)', () => {
+    // https://www.youtube.com/@SurveillanceReport
+    const parameters = { channelId: 'SurveillanceReport', channelIdType: 4 }
+    return ytch.getChannelInfo(parameters).then((data) => {
+      expect(data.authorId).toBe('UC0W_BIuwk8D0Bv4THbVZZOQ')
     })
   })
 })
